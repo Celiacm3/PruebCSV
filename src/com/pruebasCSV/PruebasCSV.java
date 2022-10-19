52b61a14d06be1c745a86dc1da743b7a29ed23da
Celia Chao <celia.chao@ufv.es>
2022-10-19 20:21:05 +0200

    prueba CSV

---
+++ src/com/pruebasCSV/PruebasCSV.java
@@ -0,0 +1,15 @@
+package com.pruebasCSV;
+
+
+/* EJEMPLO DE ACCESO A UN ARCHIVO .CSV DESDE JAVA
+   NO UTILIZA NINGÚN GESTOR DE DEPENDENCIAS
+*/
+
+public class PruebasCSV {
+    /**
+     * @author Celia Chao
+     */
+    public static void main(String[] args) {
+        
+    }
+}
